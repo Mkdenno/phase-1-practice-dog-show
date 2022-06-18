f2e33126913b9043633ff268b66ed42629bd59bd
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-18 18:02:59 +0300

    commit tree 690a82adcb1824cd663fb0c87d8ce94ddfef9bf4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetchDog()
+    
+    const tableBody=document.getElementById('table-body')
+    const tableRow=document.createElement('tr')
+    function renderDogs(dogs){
+        for( const item in dogs){
+            tableRow.innerHTML=`
+            <td>${item.name}</td>
+            <td>${item.breed}</td>
+            <td>${item.sex}</td>
+            <td><button>Edit</button></td>
+            `
+            tableBody.appendChild(tableRow);
+            console.log(item.name)
+            
+        } 
+    }
+    function fetchDog(){
+        fetch("http://localhost:3000/dogs")
+    .then(res => res.json())
+    .then(dogData=> {
 
+        dogData.forEach(dog => {
+            renderDogs(dog)
+
+            //console.log(dog)
+        });
+        
+        
+    }
+        
+    )
+    
+    }
 })
+
+
+
+
+
